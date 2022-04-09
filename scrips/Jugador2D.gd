7cb1cf3c4243fb2412b856d762feda2df6e5039d
THONYGD74 <thonygd74@gmail.com>
2022-04-09 12:16:20 -0500

    movimiento de la escalera

--- scrips/Jugador2D.gd
+++ scrips/Jugador2D.gd
@@ -3,14 +3,23 @@ extends KinematicBody2D
 var gravedad = 10
 var dir = 0
 var motion: Vector2 = Vector2()
+
 export var acc = 10
 export var ficc = 5
 export var maxspeed = 12000
 
+var subiendo: bool
+var is_ladder: bool = false
+
 onready var anim = $AnimatedSprite
 
 func _physics_process(delta):
-	motion.y += gravedad
+	
+	
+	if !is_ladder:
+		motion.y += gravedad
+	else:
+		motion.y = 0
 	
 	get_input(delta)
 	state_ma()
@@ -28,6 +37,13 @@ func get_input(delta):
 	else:
 		motion.x = move(motion.x, 0,ficc)
 		
+	if Input.is_action_pressed("ui_up") and is_ladder :
+		motion.y -= 200
+		subiendo = true
+	if Input.is_action_pressed("ui_down") and is_ladder:
+		motion.y += 200
+		subiendo = true
+	
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_floor():
 			motion.y = -200
@@ -41,12 +57,25 @@ func state_ma():
 		anim.play("run")
 	if dir != 0:
 		anim.scale.x = dir
-	if motion.y and !is_on_floor():
+	if motion.y and !is_on_floor() and !is_ladder:
 		anim.play("jump")
 
+	if !is_on_floor() and is_ladder and subiendo:
+		anim.play("ladder")
 
 func move(inicio: int, final: int, progreso: int):
 	if inicio < final:
 		return min(inicio + progreso, final)
 	else:
 		return max(inicio - progreso, final)
+
+
+func _on_ladder_body_entered(body):
+	if body.is_in_group("escaleras"):
+		is_ladder = true
+		
+
+func _on_ladder_body_exited(body):
+	if body.is_in_group("escaleras"):
+		is_ladder = false
+		subiendo = false
